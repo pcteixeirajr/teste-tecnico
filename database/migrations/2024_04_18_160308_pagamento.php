189eea1fcd2f1c404f48d284b56dc6d62d5e5b51
Paulo Cezar Moura Teixeira Junior <pcteixeirajr@gmail.com>
2024-04-18 14:25:41 -0300

    Second commit

---
+++ database/migrations/2024_04_18_160308_pagamento.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pagamento', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_cliente');
+            $table->string('cpf');
+            $table->string('descricao');
+            $table->float('valor', precision: 2);
+            $table->enum('status', ['pending', 'paid', 'expired', 'failed']);
+            $table->string('payment_method');
+            $table->date('data_pagamento');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pagamento');
+    }
+};
